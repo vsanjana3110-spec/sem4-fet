3f156e7e105aea9455b77bc268d54ce452c6000e
vsanjana3110-spec <vsanjana3110@gmail.com>
2026-05-15 21:07:26 +0530

    Update script.js

--- Assignments/script.js
+++ Assignments/script.js
@@ -1,36 +1,24 @@
 const CORRECT_PASSWORD = "admin123";
 let attempts = 3;
-
-// DO-WHILE (input handling demo)
-function getPasswordValue() {
-    let value;
-
+function getValue(){
+    let val;
     do {
-        value = document.getElementById("pass").value;
+        val = document.getElementById("pass").value;
         break;
     } while(false);
 
-    return value;
+    return val;
 }
-
-// LOGIN FUNCTION (WHILE LOOP)
 function checkPassword(){
-
     let msg = document.getElementById("msg");
-
     let i = 0;
-
     while(i < 1){
-
-        let value = getPasswordValue();
-
+        let value = getValue();
         if(value === CORRECT_PASSWORD){
-
             msg.innerText = "ACCESS GRANTED ✔";
             msg.className = "success";
-
-            // show form
-            document.getElementById("studentForm").style.display = "block";
+            // SHOW STUDENT DETAILS
+            document.getElementById("studentDetails").style.display = "block";
             break;
         }
         else{
@@ -51,21 +39,4 @@ function checkPassword(){
 
         i++;
     }
-}
-
-// STUDENT FORM FUNCTION
-function submitStudent(){
-
-    let name = document.getElementById("name").value;
-    let age = document.getElementById("age").value;
-    let course = document.getElementById("course").value;
-    let level = document.getElementById("level").value;
-
-    if(name === "" || age === ""){
-        document.getElementById("smsg").innerText = "Fill all fields";
-        return;
-    }
-
-    document.getElementById("smsg").innerText =
-        "Saved ✔ " + name + " | " + course + " | " + level;
 }
